9825e9cce627f38c991a9de4537bce3f394543f5
Leo Jensen <lion0005@zbc.dk>
2021-04-19 14:14:27 +0200

    Gui&Program

--- Hashing/Program.cs
+++ Hashing/Program.cs
@@ -22,12 +22,18 @@ namespace Hashing
                         Console.WriteLine("Choose your hash, Write the number\r\n 1) sha1 2) sha256 3) sha384 4) sha512 5) MD5");
                         choice = Convert.ToInt32(Console.ReadLine());
                         //switch case upon your second choice
-                        Console.WriteLine(SwitchCase.HashCase(choice));
+                        Console.WriteLine("Write your msg");
+                        string text = SwitchCase.HashCase(choice);
+                        Console.WriteLine("encoding");
+                        Console.WriteLine(text);
                         break;
                     case 2:
                         Console.WriteLine("Choose your Hmac hash, Write the number\r\n 1) sha1 2) sha256 3) sha384 4) sha512 5) MD5");
                         choice = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine(SwitchCase.HmachCase(choice)); 
+                        Console.WriteLine("Write your msg");
+                        Console.WriteLine("encoding");
+                        text = SwitchCase.HmachCase(choice);
+                        Console.WriteLine(text); 
                         break;
                     case 3:
                         i = 0;
--- Hashing/SwitchCase.cs
+++ Hashing/SwitchCase.cs
@@ -13,33 +13,23 @@ namespace Hashing
             switch (choice)
             {
                 case 1:
-                    Console.WriteLine("Write your msg");
                     string sha1MSG = Console.ReadLine();
-                    Console.WriteLine("encoding");
                     var sha1 = Hash.ComputeSHA1(Encoding.UTF8.GetBytes(sha1MSG));
                     return Convert.ToBase64String(sha1);
                 case 2:
-                    Console.WriteLine("Write your msg");
                     string sha256MSG = Console.ReadLine();
-                    Console.WriteLine("encoding");
                     var sha256 = Hash.ComputeSHA256(Encoding.UTF8.GetBytes(sha256MSG));
                     return Convert.ToBase64String(sha256);
                 case 3:
-                    Console.WriteLine("Write your msg");
                     string sha384MSG = Console.ReadLine();
-                    Console.WriteLine("encoding");
                     var sha384 = Hash.ComputeSHA384(Encoding.UTF8.GetBytes(sha384MSG));
                     return Convert.ToBase64String(sha384);
                 case 4:
-                    Console.WriteLine("Write your msg");
                     string sha512MSG = Console.ReadLine();
-                    Console.WriteLine("encoding");
                     var sha512 = Hash.ComputeSHA512(Encoding.UTF8.GetBytes(sha512MSG));
                     return Convert.ToBase64String(sha512);
                 case 5:
-                    Console.WriteLine("Write your msg");
                     string md5MSG = Console.ReadLine();
-                    Console.WriteLine("encoding");
                     var md5 = Hash.ComputeMD5(Encoding.UTF8.GetBytes(md5MSG));
                     return Convert.ToBase64String(md5);
             }
@@ -51,33 +41,23 @@ namespace Hashing
             switch (choice)
             {
                 case 1:
-                    Console.WriteLine("Write your msg");
                     string sha1MSG = Console.ReadLine();
-                    Console.WriteLine("encoding");
                     var sha1 = HMAC.ComputeHmachSHA1(Encoding.UTF8.GetBytes(sha1MSG), Key.GenerateKey());
                     return Convert.ToBase64String(sha1);
                 case 2:
-                    Console.WriteLine("Write your msg");
                     string sha256MSG = Console.ReadLine();
-                    Console.WriteLine("encoding");
                     var sha256 = HMAC.ComputeHmachSHA256(Encoding.UTF8.GetBytes(sha256MSG), Key.GenerateKey());
                     return Convert.ToBase64String(sha256);
                 case 3:
-                    Console.WriteLine("Write your msg");
                     string sha384MSG = Console.ReadLine();
-                    Console.WriteLine("encoding");
                     var sha384 = HMAC.ComputeHmachSHA384(Encoding.UTF8.GetBytes(sha384MSG), Key.GenerateKey());
                     return Convert.ToBase64String(sha384);
                 case 4:
-                    Console.WriteLine("Write your msg");
                     string sha512MSG = Console.ReadLine();
-                    Console.WriteLine("encoding");
                     var sha512 = HMAC.ComputeHmachSHA512(Encoding.UTF8.GetBytes(sha512MSG), Key.GenerateKey());
                     return Convert.ToBase64String(sha512);
                 case 5:
-                    Console.WriteLine("Write your msg");
                     string md5MSG = Console.ReadLine();
-                    Console.WriteLine("encoding");
                     var md5 = HMAC.ComputeHmacMD5(Encoding.UTF8.GetBytes(md5MSG), Key.GenerateKey());
                     return Convert.ToBase64String(md5);
             }
